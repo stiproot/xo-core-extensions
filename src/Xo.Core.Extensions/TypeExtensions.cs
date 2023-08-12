a3c821f73d493e82f397624d6b8c4aa90d235a83
Simon Stipcich <simon.stipcich@derivco.co.za>
2023-08-12 11:57:31 +0200

    feat:Type extension.

---
+++ src/Xo.Core.Extensions/TypeExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Xo.Core.Extensions;
+
+public static class TypeExtensions
+{
+    public static bool ImplementsIEnumerable(this Type @this)
+    {
+        return
+            @this.IsGenericType &&
+            @this.GetInterfaces()
+                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>));
+    }
+}
