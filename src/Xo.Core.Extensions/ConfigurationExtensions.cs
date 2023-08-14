77f3b599fc94e2397b335ea57d3ff0292ffe8d88
stiproot <stipcich.simon@gmail.com>
2023-08-14 18:00:03 +0200

    feat:Additional configuration registration method.

--- src/Xo.Core.Extensions/ConfigurationExtensions.cs
+++ src/Xo.Core.Extensions/ConfigurationExtensions.cs
@@ -2,15 +2,32 @@
 
 public static class ConfigurationExtensions
 {
-    public static IConfiguration AddConfiguration(this IServiceCollection @this)
+    public static IConfiguration AddConfigurations(this IServiceCollection @this,
+        in IEnumerable<string>? configurations = null
+    )
     {
-        var configuration = new ConfigurationBuilder()
+        var configurationBuilder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .Build();
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
+        if (configurations is null)
+        {
+            return @this.AddConfiguration(configurationBuilder.Build());
+        }
+
+        foreach (var config in configurations)
+        {
+            configurationBuilder.AddJsonFile(config, optional: false, reloadOnChange: true);
+        }
+
+        return @this.AddConfiguration(configurationBuilder.Build());
+    }
+
+    public static IConfiguration AddConfiguration(this IServiceCollection @this,
+        in IConfigurationRoot configuration
+    )
+    {
         @this.AddSingleton<IConfiguration>(configuration);
-
         return configuration;
     }
 }
